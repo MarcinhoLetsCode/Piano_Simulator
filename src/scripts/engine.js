99402768992822dceaad5e192b7f02e4f4db30e7
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2024-04-02 16:08:41 -0300

    Permitindo Somente Teclas Mapeadas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,6 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key")
 // console.log(pianoKeys);
 
+let mapedKeys = [];
 let audio = new Audio("./src/tunes/");
 //console.log(audio);
 
@@ -23,12 +24,23 @@ const playTune = (key) => {
 };
 
 pianoKeys.forEach((key)=>{
-    console.log(key.dataset.key);
+    //console.log(key);
+    //console.log(key.dataset.key);
     key.addEventListener("click",()=> playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
     //key.addEventListener("click",()=> console.log(key.dataset.key));
+    
+    mapedKeys.length === 17 ? console.log(mapedKeys) : null;
+
+    //console.log(mapedKeys);
 });
 
 document.addEventListener("keydown",(e) => {
     //console.log(e);
-    playTune(e.key);
+    //console.log(e.key);
+
+    if (mapedKeys.includes(e.key)) {
+        playTune(e.key);
+    }
+    
 });
